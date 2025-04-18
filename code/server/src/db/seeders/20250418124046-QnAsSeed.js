344807d34d19956f00b34591648148870cc223ec
wellgetsu <sevryukov1995@inbox.ru>
2025-04-18 15:59:12 +0300

    fix seeds

--- code/server/src/db/seeders/20250418124046-QnAsSeed.js
+++ code/server/src/db/seeders/20250418124046-QnAsSeed.js
@@ -1,115 +1,80 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-      await queryInterface.bulkInsert('QnAs', 
-        [
-          {
-            id: 1,
-            title: "Какой фильм выиграл Оскар за лучший фильм в 2020 году?",
-            answers: [
-              { id: "1", text: "1917", guess: false },
-              { id: "2", text: "Паразиты", guess: true },
-              { id: "3", text: "Джокер", guess: false },
-            ],
-            topic_id: 1,
-          },
-          {
-            id: 2,
-            title: "Кто сыграл главную роль в фильме 'Титаник'?",
-            answers: [
-              { id: "1", text: "Брэд Питт", guess: false },
-              { id: "2", text: "Леонардо ДиКаприо", guess: true },
-              { id: "3", text: "Джонни Депп", guess: false },
-            ],
-            topic_id: 1,
-          },
-          {
-            id: 3,
-            title: "Какой режиссер снял фильм 'Психо'?",
-            answers: [
-              { id: "1", text: "Стэнли Кубрик", guess: false },
-              { id: "2", text: "Альфред Хичкок", guess: true },
-              { id: "3", text: "Фрэнсис Форд Коппола", guess: false },
-            ],
-            topic_id: 1,
-          },
-          {
-            id: 4,
-            title: "Какой фильм является первой частью трилогии 'Властелин колец'?",
-            answers: [
-              { id: "1", text: "Две крепости", guess: false },
-              { id: "2", text: "Возвращение короля", guess: false },
-              { id: "3", text: "Братство кольца", guess: true },
-            ],
-            topic_id: 1,
-          },
-          {
-            id: 5,
-            title: "Кто исполнил роль Джокера в фильме 2019 года?",
-            answers: [
-              { id: "1", text: "Хоакин Феникс", guess: true },
-              { id: "2", text: "Джаред Лето", guess: false },
-              { id: "3", text: "Кристофер Нолан", guess: false },
-            ],
-            topic_id: 1,
-          },
-          {
-            id: 6,
-            title: "Какой анимационный фильм студии Pixar вышел в 2003 году?",
-            answers: [
-              { id: "1", text: "В поисках Немо", guess: true },
-              { id: "2", text: "История игрушек", guess: false },
-              { id: "3", text: "Суперсемейка", guess: false },
-            ],
-            topic_id: 1,
-          },
-          {
-            id: 7,
-            title: "Какой фильм стал самым кассовым в истории на 2021 год?",
-            answers: [
-              { id: "1", text: "Мстители: Финал", guess: false },
-              { id: "2", text: "Аватар", guess: true },
-              { id: "3", text: "Титаник", guess: false },
-            ],
-            topic_id: 1,
-          },
-          {
-            id: 8,
-            title: "Кто является режиссером фильма 'Крестный отец'?",
-            answers: [
-              { id: "1", text: "Фрэнсис Форд Коппола", guess: true },
-              { id: "2", text: "Мартин Скорсезе", guess: false },
-              { id: "3", text: "Стивен Спилберг", guess: false },
-            ],
-            topic_id: 1,
-          },
-          {
-            id: 9,
-            title: "Какой фильм рассказывает о жизни и карьере певицы Уитни Хьюстон?",
-            answers: [
-              { id: "1", text: "Уитни", guess: false },
-              { id: "2", text: "Светлая звезда", guess: false },
-              { id: "3", text: "Я хочу танцевать с тобой", guess: true },
-            ],
-            topic_id: 1,
-          },
-          {
-            id: 10,
-            title:
-              "Какой фильм стал первым в истории, получившим Оскар за лучший анимационный фильм?",
-            answers: [
-              { id: "1", text: "В поисках Немо", guess: false },
-              { id: "2", text: "Шрек", guess: true },
-              { id: "3", text: "История игрушек", guess: false },
-            ],
-            topic_id: 1,
-          },
-        ], {});
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "QnAs",
+      [
+        {
+          id: 1,
+          question: "Какой фильм выиграл Оскар за лучший фильм в 2020 году?",
+          answer: "Паразиты",
+          topic_id: 1,
+        },
+        {
+          id: 2,
+          question: "Кто сыграл главную роль в фильме 'Титаник'?",
+          answer: "Леонардо ДиКаприо",
+          topic_id: 1,
+        },
+        {
+          id: 3,
+          question: "Какой режиссер снял фильм 'Психо'?",
+          answer: "Альфред Хичкок",
+          topic_id: 1,
+        },
+        {
+          id: 4,
+          question:
+            "Какой фильм является первой частью трилогии 'Властелин колец'?",
+          answer: "Братство кольца",
+          topic_id: 1,
+        },
+        {
+          id: 5,
+          question: "Кто исполнил роль Джокера в фильме 2019 года?",
+          answer: "Хоакин Феникс",
+          topic_id: 1,
+        },
+        {
+          id: 6,
+          question: "Какой анимационный фильм студии Pixar вышел в 2003 году?",
+          answer: "В поисках Немо",
+          topic_id: 1,
+        },
+        {
+          id: 7,
+          question: "Какой фильм стал самым кассовым в истории на 2021 год?",
+          answer: "Аватар",
+          topic_id: 1,
+        },
+        {
+          id: 8,
+          question: "Кто является режиссером фильма 'Крестный отец'?",
+          answer: "Фрэнсис Форд Коппола",
+          topic_id: 1,
+        },
+        {
+          id: 9,
+          question:
+            "Какой фильм рассказывает о жизни и карьере певицы Уитни Хьюстон?",
+          answer: "Я хочу танцевать с тобой",
+          topic_id: 1,
+        },
+        {
+          id: 10,
+          question:
+            "Какой фильм стал первым в истории, получившим Оскар за лучший анимационный фильм?",
+          answer: "Шрек",
+          topic_id: 1,
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-     await queryInterface.bulkDelete('QnAs', null, {});
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("QnAs", null, {});
+  },
 };
